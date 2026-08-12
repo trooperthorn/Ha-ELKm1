df7b9b17ab285dfe45e6a82d5a66a90549794b44
trooperthorn <8163700+trooperthorn@users.noreply.github.com>
2026-08-12 08:13:18 -0500

    Create __init__.py for Elk-M1 helpers package
    Added __init__.py to mark the helpers package for Elk-M1 integration.

---
+++ custom_components/elkm1/helpers/__init__.py
@@ -0,0 +1,5 @@
+"""Helpers for Elk-M1 integration."""
+# Usually empty - just marks this as a package
+# Optionally, you could add convenience imports:
+# from .usb_discovery import discover_elk_ports
+# from .serial_queue import ElkSerialQueue
