a1a9ff656c03dca487eb8c28f1149911b6033bce
trooperthorn <8163700+trooperthorn@users.noreply.github.com>
2026-08-12 08:14:46 -0500

    Add imports and update __all__ in init.py
    Added convenience imports and updated __all__ for exports.

--- custom_components/elkm1/helpers/__init__.py
+++ custom_components/elkm1/helpers/__init__.py
@@ -3,3 +3,7 @@
 # Optionally, you could add convenience imports:
 # from .usb_discovery import discover_elk_ports
 # from .serial_queue import ElkSerialQueue
+from .usb_discovery import discover_elk_ports, probe_serial_port
+from .serial_queue import ElkSerialQueue
+
+__all__ = ["discover_elk_ports", "probe_serial_port", "ElkSerialQueue"]
