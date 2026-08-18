9a38fc4e6fd335e7e41533c066661c1d45e8867d
trooperthorn <8163700+trooperthorn@users.noreply.github.com>
2026-08-18 11:48:36 -0500

    Add helper functions for Elk-M1 integration

--- custom_components/elkm1/helpers/__init__.py
+++ custom_components/elkm1/helpers/__init__.py
@@ -1,9 +1,32 @@
 """Helpers for Elk-M1 integration."""
-# Usually empty - just marks this as a package
-# Optionally, you could add convenience imports:
-# from .usb_discovery import discover_elk_ports
-# from .serial_queue import ElkSerialQueue
+
+from __future__ import annotations
+
+from .panel_settings import (
+    check_panel_version,
+    check_required_settings,
+    enable_required_settings,
+    verify_panel_configuration,
+)
 from .serial_queue import ElkSerialQueue
+from .troublestatus import (
+    get_critical_troubles,
+    get_trouble_status_string,
+    has_troubles,
+    parse_trouble_status,
+)
 from .usb_discovery import discover_elk_ports, probe_serial_port
 
-__all__ = ["ElkSerialQueue", "discover_elk_ports", "probe_serial_port"]
+__all__ = [
+    "ElkSerialQueue",
+    "check_panel_version",
+    "check_required_settings",
+    "discover_elk_ports",
+    "enable_required_settings",
+    "get_critical_troubles",
+    "get_trouble_status_string",
+    "has_troubles",
+    "parse_trouble_status",
+    "probe_serial_port",
+    "verify_panel_configuration",
+]
