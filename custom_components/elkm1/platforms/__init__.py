34288d1e59d92051bca87bcf37178abf0a3fa2a8
trooperthorn <8163700+trooperthorn@users.noreply.github.com>
2026-08-18 22:28:58 -0500

    Delete custom_components/elkm1/platforms directory

--- custom_components/elkm1/platforms/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Platforms and setup utilities for Elk-M1 integration."""
-
-from __future__ import annotations
-
-from .setup_wizard import run_panel_setup_wizard
-
-__all__ = ["run_panel_setup_wizard"]
